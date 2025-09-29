8bc62dec42e8ded0f9f612ab75b7d62aea83d415
mk-samoilov <me@mk-samoilov.ru>
2025-09-29 20:12:52 +0300

    added build_conf_of_pdb func

--- serv/db_api/main_db_api.py
+++ serv/db_api/main_db_api.py
@@ -3,6 +3,18 @@ from configparser import ConfigParser
 from .databaser import PDB
 
 
+def build_conf_of_pdb(app_conf: ConfigParser):
+    db_conf = app_conf["db"]
+
+    user = db_conf["role"]
+    user_password = db_conf["role_passwd"]
+    host = db_conf["db_host"]
+    port = db_conf["db_port"]
+    database_name = db_conf["db_name"]
+
+    return None, host, port, database_name, user, user_password, None, False
+
+
 class MainAppDatabaseAPI:
     def __init__(self, app_conf: ConfigParser):
-            self.db = PDB()
+        self.db = PDB(*build_conf_of_pdb(app_conf=app_conf))
